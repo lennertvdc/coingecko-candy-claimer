0d1150011d7ecf95670aac49cd994b39e8ffb954
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2021-08-02 18:12:16 +0200

    Create simple script for claiming candies

---
+++ candy-claimer.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+const URL = 'https://www.coingecko.com/account/candy?locale=en';
+const CHROME_OPTIONS = {
+	headless: process.env.NODE_ENV !== 'development',
+	slowMo: 10,
+	devtools: true,
+	defaultViewport: {
+		width: 1920,
+		height: 1080
+	}
+};
+
+async function claimCandy(account) {
+	const browser = await puppeteer.launch(CHROME_OPTIONS);
+	browser.defaultBrowserContext();
+	const page = await browser.newPage();
+	await page.goto(URL);
+
+	await page.type('#user_email', account.email, {delay: 30});
+	await page.type('#user_password', account.password, {delay: 30});
+	await page.keyboard.press('Enter');
+
+	await page.waitForNavigation();
+	try {
+		await page.click('input.collect-candy-button');
+		await browser.close();
+	} catch (e) {
+		await browser.close();
+		throw 'Can not claim candies, candies are already claimed.';
+	}
+
+}
+
+module.exports = {claimCandy};
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+require('dotenv').config()
+const scraper = require('./candy-claimer');
+const cron = require('node-cron');
+
+(async () => {
+	const ACCOUNTS = [{
+		email: process.env.EMAIL,
+		password: process.env.PASSWORD
+	}];
+
+	try {
+		if (process.env.NODE_ENV === 'development') {
+			await scraper.claimCandy(ACCOUNTS[0]);
+		} else {
+			cron.schedule('* * * * *', async () => {
+				try {
+					await scraper.claimCandy(ACCOUNTS[0])
+				}catch (e) {
+					console.log(e);
+				}
+			}, {});
+		}
+	} catch (e) {
+		console.log(e);
+	}
+
+})();
+
